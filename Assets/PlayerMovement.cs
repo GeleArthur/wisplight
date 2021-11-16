9cc28a878b25c02bdb513f780d2da0c61f482c53
arthur <geleminecrafter@gmail.com>
2021-11-16 23:02:25 +0100

    Added mats. Very simple player move
    de rb heeft geen friction of iets wat hem stopt behalven een muur. hier door kan ik meer controllen hebben

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float XInput;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        XInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(XInput,0,0),ForceMode.Acceleration);
     }
 }
