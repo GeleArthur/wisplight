539a6d1a478c2b5f4cc9615b4306b1c2fd75bbe3
The-White-Code <pkramer@zeelandnet.nl>
2021-11-22 09:04:59 +0100

    new movement

--- Assets/Scripts/PatrickMovement/PlayerController.cs
+++ Assets/Scripts/PatrickMovement/PlayerController.cs
@@ -8,13 +8,13 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float speed = 10f;
     [SerializeField] float acceration = 10f;
+    [SerializeField] float jumpForce = 15f;
     [SerializeField] float gravity = 9.8f;
-    [SerializeField] float sensativity = 10f;
+    [SerializeField] float sensativity = 0.75f;
 
     CharacterController characterController;
     private Vector2 velocity = new Vector2();
-    private Vector2 mouseDir = Vector2.up;
-    private Vector2 lastMousePos = new Vector2();
+    private Vector2 mouseDir = Vector2.down;
 
     private void Awake()
     {
@@ -25,22 +25,22 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        lastMousePos = Input.mousePosition;
     }
 
     private void Update()
     {
-        Vector2 currentMousePos = Input.mousePosition;
-        if ((currentMousePos - lastMousePos).magnitude >= sensativity)
+        mouseDir += new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * sensativity;
+        mouseDir.Normalize();
+        if(Input.GetMouseButtonUp(0))
         {
-            mouseDir = (currentMousePos - lastMousePos).normalized;
-            lastMousePos = Input.mousePosition;
+            velocity += -mouseDir * jumpForce;
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         if (Physics.Raycast(transform.position + Vector3.down * characterController.bounds.extents.y, Vector3.down, 0.1f))
         {
             velocity.x = Mathf.MoveTowards(velocity.x, Input.GetAxisRaw("Horizontal") * speed, acceration * Time.deltaTime);
