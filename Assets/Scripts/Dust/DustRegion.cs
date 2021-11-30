b5f19c476db132db324f73932d17181d8217ad07
The-White-Code <pkramer@zeelandnet.nl>
2021-11-30 11:07:56 +0100

    Update the Dust to stick to the ground

--- Assets/Scripts/Dust/DustRegion.cs
+++ Assets/Scripts/Dust/DustRegion.cs
@@ -18,7 +18,16 @@ public class DustRegion : MonoBehaviour
 
     public void AddDustPile(float amount)
     {
-        Instantiate(dustPrefab, transform.position + bounds.GetRandomPoint(), Quaternion.identity, transform).GetComponent<DustPile>().SetAmount(amount);
+        Vector3 randomPosition = transform.position + bounds.GetRandomPoint();
+        float distance = randomPosition.y - (transform.position.y + bounds.center.y - bounds.extents.y);
+        Debug.Log(distance);
+        RaycastHit hit;
+        if(Physics.Raycast(randomPosition, Vector3.down, out hit, distance))
+        {
+            Quaternion rot = Quaternion.LookRotation(Vector3.forward, hit.normal);
+            Instantiate(dustPrefab, hit.point, rot, transform).GetComponent<DustPile>().SetAmount(amount);
+        }
+        //Instantiate(dustPrefab, transform.position + bounds.GetRandomPoint(), Quaternion.identity, transform).GetComponent<DustPile>().SetAmount(amount);
     }
 
     public void OnDrawGizmos()
