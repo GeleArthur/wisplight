a041e64ff3f16b7486fd4302324ee720db2165f0
The-White-Code <pkramer@zeelandnet.nl>
2021-12-01 14:03:44 +0100

    Dust overhaul!!

--- Assets/Scripts/Dust/DustPile.cs
+++ Assets/Scripts/Dust/DustPile.cs
@@ -6,20 +6,48 @@ public class DustPile : MonoBehaviour, DustCleanedInterface
 {
     [SerializeField] private float amount = 0;
     [SerializeField] private float offset = 0.5f;
+    [SerializeField] private GameObject[] models = new GameObject[] { };
+    [SerializeField] private float maxDust = 50f;
+    [SerializeField] private float cleanAnimationTime = 5f;
+
+    private float lifeLeft = 0f;
+    private bool cleaned = false;
 
     private void Awake()
     {
-        transform.position += transform.up * offset;
+
+    }
+
+    public void Update()
+    {
+        if (cleaned)
+        {
+            lifeLeft -= Time.deltaTime;
+            transform.localScale = Vector3.one * (lifeLeft / cleanAnimationTime);
+            if (lifeLeft <= 0f)
+                Destroy(this);
+        }
     }
 
     public void SetAmount(float newAmount)
     {
         amount = newAmount;
+        for (int i = 0; i < models.Length; i++)
+            models[i].SetActive(false);
+
+        float dustPerModel = maxDust / models.Length;
+        int thing = Mathf.FloorToInt(amount / dustPerModel);
+        models[thing].SetActive(true);
     }
 
     public void Cleaned()
     {
-        DustManager.Singleton.RemoveDust(amount);
-        Destroy(gameObject);
+        if (!cleaned)
+        {
+            DustManager.Singleton.RemoveDust(amount);
+            GetComponent<BoxCollider>().enabled = false;
+            cleaned = true;
+            lifeLeft = cleanAnimationTime;
+        }
     }
 }
