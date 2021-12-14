de43a5e9458cbefcfbffaf3bb6367ffd0c0d0c16
arthur <geleminecrafter@gmail.com>
2021-12-14 12:55:33 +0100

    Added audio. Removed 2 camera. More Shawdow fixes. Rebuild light map

--- Assets/Scripts/ArthurMovement/ShadowProjector.cs
+++ Assets/Scripts/ArthurMovement/ShadowProjector.cs
@@ -7,6 +7,7 @@ public class ShadowProjector : MonoBehaviour
 {
     public Transform player;
     public LayerMask layers;
+    public float offset;
 
     private Vector3 castPos1;
     private Vector3 castPos2;
@@ -14,7 +15,8 @@ public class ShadowProjector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player ??= transform.parent;
+        if(player == null)
+            player = transform.parent;
     }
 
     // Update is called once per frame
@@ -35,9 +37,8 @@ public class ShadowProjector : MonoBehaviour
         if (cast1 == true) shadowLength = hitInfo1.distance;
         if (cast2 == true && hitInfo2.distance < shadowLength) shadowLength = hitInfo2.distance;
 
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, shadowLength + 0.1f);
-        transform.position = player.position - new Vector3(0, bounds.size.y / 2f, 0) -
-                             new Vector3(0, shadowLength / 2, 0);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, shadowLength);
+        transform.position = player.position - new Vector3(0, bounds.size.y / 2f, 0) - new Vector3(0, shadowLength / 2-offset, 0);
 
         // if (Physics.Raycast(player.position, Vector3.down, out var hitInfo, Single.MaxValue, layers))
         // {
@@ -45,11 +46,15 @@ public class ShadowProjector : MonoBehaviour
         // }
     }
 
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
+        if(player == null)
+            player = transform.parent;
         Bounds wut = player.GetComponent<PlayerMovement>().characterModel.GetComponent<Renderer>().bounds;
 
         Gizmos.DrawRay(castPos1, Vector3.down);
         Gizmos.DrawRay(castPos2, Vector3.down);
     }
+#endif
 }
