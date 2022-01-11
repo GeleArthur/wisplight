747c28c75d51e68c59fddc8bece6cd6c540f2ade
The-White-Code <pkramer@zeelandnet.nl>
2022-01-11 12:32:04 +0100

    removed debug and changed lighting

--- Assets/Scripts/LightMapShit.cs
+++ Assets/Scripts/LightMapShit.cs
@@ -4,29 +4,27 @@ using UnityEngine;
 
 public class LightMapShit : MonoBehaviour
 {
-    //[SerializeField] float multiplier = 2f;
     [SerializeField] Vector2 minMax = Vector2.up;
+    [SerializeField] float changeSpeed = 1f;
 
     GameObject player;
-    Light light;
+    Light characterLight;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        light = GetComponent<Light>();
+        characterLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        Physics.Raycast(transform.position, (player.transform.position - transform.position).normalized, out hit, 100f, (1 << 0), QueryTriggerInteraction.UseGlobal);
+        Physics.Raycast(Camera.main.transform.position, (player.transform.position - Camera.main.transform.position).normalized, out hit, 100f, (1 << 0), QueryTriggerInteraction.UseGlobal);
 
-        Debug.Log(hit.transform.gameObject.GetComponent<Renderer>().lightmapIndex);
+        float grayscale = LightmapSettings.lightmaps[hit.transform.gameObject.GetComponent<Renderer>().lightmapIndex].lightmapColor.GetPixelBilinear(hit.lightmapCoord.x, hit.lightmapCoord.y).grayscale;
 
-        Color color = LightmapSettings.lightmaps[hit.transform.gameObject.GetComponent<Renderer>().lightmapIndex].lightmapColor.GetPixelBilinear(hit.lightmapCoord.x, hit.lightmapCoord.y);
-
-        light.intensity = minMax.x + color.grayscale * (minMax.y - minMax.x);
+        characterLight.intensity = Mathf.MoveTowards(characterLight.intensity, minMax.x + grayscale * (minMax.y - minMax.x), Time.deltaTime * changeSpeed * (minMax.y - minMax.x));
     }
 }
